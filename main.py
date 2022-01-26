a0af3bb4fd715aa6d175541845b37c4466bd9c28
2landhnal <93079233+2landhnal@users.noreply.github.com>
2022-01-26 16:48:52 +0700

    postgres

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ Bootstrap(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL1', 'sqlite:///todos.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 db = SQLAlchemy(app)
 
 login_manager = LoginManager()
